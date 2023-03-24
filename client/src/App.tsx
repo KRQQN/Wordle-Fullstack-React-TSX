399b0f349f1cf46e456b6b1a5f25750df139254c
Rkron91 <robin.kron@lerniastudent.se>
2023-03-24 16:14:58 +0100

    templatePage + Fa + counter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(5);
 
   return (
-    <h1 className="text-6xl font-bold underline text-center">Hello world!</h1> 
-  )
+    <>
+      <h1>Word Length</h1>
+      <div className="flex justify-center ">
+        <button className="w-20 leading-none rounded-sm font-extrabold text-2xl bg-orange-400" 
+          onClick={() => setCount(count - 1)}
+          >
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+
+        <p className="w-12 bg-white font-bold text-lg">{count}</p>
+
+        <button className="w-20  rounded-sm  bg-orange-400 flex items-center justify-center " 
+          onClick={() => setCount(count + 1)}
+          >
+          <FontAwesomeIcon icon={faPlus} className="text-lg" />
+        </button>
+      </div>
+
+      <h1 className="h-80 mt-5 text-xl bg-yellow-300 font-bold text-center">APP CONTENT</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
